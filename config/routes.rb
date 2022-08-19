c98dc743d5073573c3f4f4c735bd604c015bf3b7
Kelvin <kelvinc1987@yahoo.com>
2022-08-19 12:37:27 -0400

    added api call

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-  get 'topics/index/:id', to: 'topics#index'
+  
+  get '/topics', to: 'topics#index'
+  get '/test', to: 'topics#test'
+  get '/test/:id', to: 'topics#show'
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
