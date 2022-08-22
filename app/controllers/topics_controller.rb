dcac7feaf7d71519c605309f0f249a63f4643fe3
Kelvin <kelvinc1987@yahoo.com>
2022-08-22 17:28:43 -0400

    fixed env

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,13 +12,13 @@ class TopicsController < ApplicationController
   def show
     key = ENV['MY_BEARER_TOKEN']
     @topic = Topic.find(params[:id])
-    @response = HTTParty.get("https://api.twitter.com/2/tweets/search/recent?query=#{@topic.hashtag}&max_results=10", headers: {'Authorization' =>" #{key}"})
+    @response = HTTParty.get("https://api.twitter.com/2/tweets/search/recent?query=#{@topic.hashtag}&max_results=10", headers: {'Authorization' =>"Bearer #{key}"})
   
   end
 
   def usertopic 
     key = ENV['MY_BEARER_TOKEN']
     @userhash = params.require(:usertopic)
-    @response = HTTParty.get("https://api.twitter.com/2/tweets/search/recent?query=#{@userhash}&max_results=10", headers: {'Authorization' => "#{key}"})
+    @response = HTTParty.get("https://api.twitter.com/2/tweets/search/recent?query=#{@userhash}&max_results=10", headers: {'Authorization' => "Bearer #{key}"})
   end
 end
